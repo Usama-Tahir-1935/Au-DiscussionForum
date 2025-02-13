fb188610acce825d508305cb5905c132fbc9822c
Malik Usama <malikusama1935@gmail.com>
2025-02-14 01:51:34 +0500

    End of section 9

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,16 +1,12 @@
-using System;
-using API.Data;
 using API.DTOs;
-using API.Entities;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Authorize]
+    // [Authorize]
     public class UsersController : BaseApiController
     {
         private readonly IUserRepository _userRepository;
