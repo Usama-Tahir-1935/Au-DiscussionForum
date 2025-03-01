a49be5498366b22c23b65b919b96f6ccbe6d10b9
Malik Usama <malikusama1935@gmail.com>
2025-03-01 20:32:41 +0500

    End of section 18

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -30,6 +30,7 @@ import { PhotoManagementComponent } from './admin/photo-management/photo-managem
 import { RolesModalComponent } from './modals/roles-modal/roles-modal.component';
 import { CustomRouteReuseStrategy } from './_services/CustomRouteReuseStrategy';
 import { RouteReuseStrategy } from '@angular/router';
+import { ConfirmDialogComponent } from './modals/confirm-dialog/confirm-dialog.component';
 
 @NgModule({
   declarations: [
@@ -53,6 +54,7 @@ import { RouteReuseStrategy } from '@angular/router';
     UserManagementComponent,
     PhotoManagementComponent,
     RolesModalComponent,
+    ConfirmDialogComponent,
   ],
   imports: [
     BrowserModule,
